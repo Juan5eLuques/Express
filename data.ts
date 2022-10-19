f0d62bd2cd6328acc8ed57d785c427798f9d91ae
Juanse <luquesjuanse.10@gmail.com>
2022-10-18 21:18:36 -0300

    first EndPoints, Insert, Post, Put, Delete

---
+++ data.ts
@@ -0,0 +1,29 @@
+import { Product } from "types/Product"
+
+export const data : Product [] = [
+{
+    id:1,
+    name:'Televisor',
+    marca : 'LG',
+    stock : 4  
+},
+{
+    id:2,
+    name:'PC',
+    marca : 'HP',
+    stock : 2
+},
+{
+    id:3,
+    name:'Tostadora',
+    marca : 'Express',
+    stock : 5
+},
+{
+    id:4,
+    name:'Celular',
+    marca : 'Iphone',
+    stock : 4  
+},
+
+]
--- index.ts
+++ index.ts
@@ -1,11 +1,72 @@
-import express from 'express'
+import express from "express";
+import bodyParser from "body-parser";
+import { Product } from "types/Product";
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res) => {
-    res.send('Hello world')
-})
+app.use(bodyParser.json());
 
-app.listen(3000, () => {
-    console.log('Estoy escuchando en el puerto 3000')
-})
+
+const product : Product[] =  [
+    {
+        id:1,
+        name:'Televisor',
+        marca : 'LG',
+        stock : 4  
+    },
+    {
+        id:2,
+        name:'PC',
+        marca : 'HP',
+        stock : 2
+    },
+    {
+        id:3,
+        name:'Tostadora',
+        marca : 'Express',
+        stock : 5
+    },
+    {
+        id:4,
+        name:'Celular',
+        marca : 'Iphone',
+        stock : 4  
+    },
+    
+    ]
+
+app.get("/product", (req, res) => {
+  res.status(200).json(product);
+});
+
+app.post("/product", (req, res) => {
+  const { name, marca, stock } = req.body;
+  try {
+    if (!name || !marca) throw new Error("che pasame el name y marca");
+    product.push({ id: new Date().getTime(), name, marca, stock });
+    res.status(200).json(...product.slice(-1));
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
+  }
+});
+
+app.put("/product", (req, res) => {
+  const { name, marca, id } = req.body;
+  const index = product.findIndex(product => product.id === id);
+
+  if (index === -1)
+    return res.status(400).json({ message: "Product not found - 400" });
+
+  product[index] = { ...product[index], name, marca };
+  res.status(200).json(product[index]);
+});
+
+app.delete("/product/id", (req, res) => {
+    const { id } = req.body
+    const index = product.findIndex(product => product.id === id);
+    if (index === -1) return res.status(400).json({message : "El ID ingresado no se encuentra en la BD  "})
+    res.status(200).json(product.splice(index,1))
+
+});
+
+app.listen(3000);
